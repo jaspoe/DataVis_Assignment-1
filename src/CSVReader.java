e1d6d225d6ec119df9b16f6229843ecf54a0f039
jaspoe <jana.spoerri@uzh.ch>
2016-11-13 20:17:56 +0100

    createNewArea
    create new area complete

--- src/CSVReader.java
+++ src/CSVReader.java
@@ -41,61 +41,52 @@ public class CSVReader {
 		return counter;
 	}
 
-//	public ArrayList<Plot1DataObject> dataGenPlot1(){
-//		String csvFile = "D:/eclipse/workspace/DataVis_Diagrams/src/res/bevgeburtenjahrgeschlquartstz.csv";
-//		String line = "";
-//		String cvsSplitBy = ",";
-//		int counterArea = 0;
-//		final String YEAR = "2015";
-//		String area = "";
-//		
-//		ArrayList<Plot1DataObject> returnData = new ArrayList<Plot1DataObject>();
-//
-//		try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
-//
-//			while ((line = br.readLine()) != null) {
-//
-//				// use comma as separator
-//				String[] data = line.split(cvsSplitBy);
-//
-//				
-//				if(data[0].equals(YEAR)){
-//					//create new Area and increase Birth counter (male or female)
-//					if(!data[7].equals(area)){
-//						area = data[7];
-//						counterArea = counterArea +1;
-//						Plot2DataObject newArea = new Plot2DataObject(area, 0, 0);
-//						returnData.add(newArea);
-//						//increase male counter
-//						if(data[1].equals("1")){
-//							returnData.get(counterArea).counterMale++;
-//						}
-//						//increase female counter
-//						else{
-//							returnData.get(counterArea).counterFemale++;
-//						}
-//						
-//					}
-//					// increase Birth counter (male or female)
-//					else{
-//					
-//						//increase male counter
-//						if(data[1].equals("1")){
-//							returnData.get(counterArea).counterMale++;
-//						}
-//						//increase female counter
-//						else{
-//							returnData.get(counterArea).counterFemale++;
-//					}
-//				}
-//			}
-//				System.out.println("Area: " +returnData.get(counterArea).area);
-//				System.out.print(" Male: " + returnData.get(counterArea).counterMale);
-//				System.out.print(" Female: " + returnData.get(counterArea).counterFemale);
-//		}} catch (IOException e) {
-//			e.printStackTrace();
-//		}
-//
-//		return returnData;
-//	}
+	public ArrayList<Plot1DataObject> dataGenPlot1(){
+		String csvFile = "D:/eclipse/workspace/DataVis_Diagrams/src/res/bevgeburtenjahrgeschlquartstz.csv";
+		String line = "";
+		String cvsSplitBy = ",";
+		int counterArea = -1;
+		int counterMale = 0;
+		int counterFemale = 0;
+		final String YEAR = "2015";
+		String area = "";
+		
+		ArrayList<Plot1DataObject> returnData = new ArrayList<Plot1DataObject>();
+
+		try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
+
+			while ((line = br.readLine()) != null) {
+
+				// use comma as separator
+				String[] data = line.split(cvsSplitBy);
+
+				//select all Births' in year 2015
+				if(data[0].equals(YEAR)){
+					//check if a new area has to be added to arrayList
+					if(!data[6].equals(area)){
+						counterArea++;
+						area = data[6];
+						//add all the male births'
+						if(data[2].equals("\"M\"")){
+							counterMale = Integer.parseInt(data[7]);
+							counterFemale = 0;
+						}
+						//add all female births'
+						else{
+							counterMale = 0;
+							counterFemale = Integer.parseInt(data[7]);
+						}
+						Plot1DataObject addArea = new Plot1DataObject(area, counterMale, counterFemale);
+						returnData.add(addArea);
+						System.out.println("area: " + returnData.get(counterArea).area + " male: " + returnData.get(counterArea).counterMale
+								+ " female: " + returnData.get(counterArea).counterFemale);
+					}
+				}
+
+		}} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return returnData;
+	}
 }
--- src/Main.java
+++ src/Main.java
@@ -3,20 +3,20 @@ import org.jfree.ui.RefineryUtilities;
 public class Main {
 
 	public static void main(String[] args) {
-		//CSVReader reader = new CSVReader();
-		//Create Plot 1
+		CSVReader reader = new CSVReader();
+//		//Create Plot 1
 //		final Plot1 demo = new Plot1("Plot1");
 //        demo.pack();
 //        RefineryUtilities.centerFrameOnScreen(demo);
 //        demo.setVisible(true);
-        //reader.dataGenPlot1();
+        reader.dataGenPlot1();
 		
 		
 		//Create Plot 2
 		
-		Plot2 plot2 = new Plot2("Comparison", "Which operating system are you using?");
-        plot2.pack();
-        plot2.setVisible(true);
+//		Plot2 plot2 = new Plot2("Comparison", "Which operating system are you using?");
+//        plot2.pack();
+//        plot2.setVisible(true);
 
     }
 }
